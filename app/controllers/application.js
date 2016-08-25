aa696602089f299695613073d354ef3f7f30c2ba
Sarah <sarahkane682@yahoo.com>
2016-08-25 09:00:55 -0700

    sound is working

---
+++ app/controllers/application.js
@@ -0,0 +1,13 @@
+import Em from 'ember';
+
+
+export default Em.Controller.extend({
+  audioService: Em.inject.service(),
+  actions:{
+    play(sound){
+      this.get('audioService').play(sound);
+
+    }
+  }
+
+});
---
+++ app/services/audio-service.js
@@ -0,0 +1,25 @@
+import Em from 'ember';
+
+export default Em.Service.extend({
+  init() {
+    let howl = new Howl({
+      src: ['sprite.mp3'],
+      sprite: {
+        car: [0, 300],
+      }
+    });
+
+    this.set('howl', howl);
+  },
+
+  sounds: Em.computed('howl', function() {
+    let howl = this.get('howl');
+
+    return Object.keys(howl._sprite);
+  }),
+
+  play(sound) {
+    let howl = this.get('howl');
+    howl.play(sound);
+  }
+});
