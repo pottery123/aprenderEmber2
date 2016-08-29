624f588e602e8a975a18f60b0ecd4fc65686f8fa
Sarah <sarahkane682@yahoo.com>
2016-08-28 22:06:51 -0700

    working on sprites it is tedious

--- app/services/audio-service.js
+++ app/services/audio-service.js
@@ -3,13 +3,39 @@ import Em from 'ember';
 export default Em.Service.extend({
   init() {
     let howl = new Howl({
-      src: ['/sprite2.mp3'],
+      src: ['/spanish.mp3'],
       sprite: {
-        seatbelt:       [0,1000 ],
-        frontSeat:      [1000,2000 ],
-        steeringWheel:  [3000, 4000],
-        windoW:         [300, 400],
-        car:            [400,500 ]
+        car:           [34050, 2000 ],
+        seatbelt:      [37190, 4317 ],
+        windoWl:       [42190, 3000 ],
+        backseat:      [47060, 2500 ],
+        frontseat:     [51140, 3000 ],
+        livingroom:    [56200,1000 ],
+        couch:         [59500,2000 ],
+        windoW:        [103010,2000 ],
+        chair:         [, ],
+        curtain:       [, ],
+        lamp:          [, ],
+        table:         [, ],
+        spoon:         [, ],
+        plate:         [, ],
+        pitcher:       [, ],
+        fork:          [, ],
+        tablecloth:    [, ],
+        cup:           [, ],
+        napkin:        [, ],
+        food:          [, ],
+        water:         [, ],
+        juice:         [, ],
+        store:         [, ],
+        shoppingcart:  [, ],
+        fruit:         [, ],
+        vegetables:    [, ],
+        meat:          [, ],
+        cannedfoods:   [, ],
+        milk:          [, ],
+        creditcard:    [, ]
+
       },
 });
 
